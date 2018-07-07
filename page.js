839e650b947250b285ec1947be49c85875772963
notsilverman <birchba@gmail.com>
2018-07-06 23:40:45 -0500

    Formatted the portfolio, zoey, and contact pages

---
+++ page.js
@@ -0,0 +1,77 @@
+const pic1 = $('#zoAndMe')
+const pic2 = $('#halloweenZo')
+const pic3 = $('#parkZo')
+const pic4 = $('#sleeping')
+const pic5 = $('#birthdayZo')
+const pic6 = $('#napZo')
+
+
+
+setInterval(hidePic1, 4000)
+setInterval(hidePic2, 6000)
+setInterval(hidePic3, 5000)
+setInterval(hidePic4, 9000)
+setInterval(hidePic5, 10000)
+setInterval(hidePic6, 5000)
+
+
+
+setInterval(showPic1, 8000);
+setInterval(showPic2, 12000);
+setInterval(showPic3, 10000);
+setInterval(showPic4, 18000);
+setInterval(showPic5, 20000);
+setInterval(showPic6, 10000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//look into prototype functions
+function showPic1(){
+ pic1.show()
+}
+function showPic2(){
+    pic2.show()
+   }
+   function showPic3(){
+    pic3.show()
+   }
+   function showPic4(){
+    pic4.show()
+   }
+   function showPic5(){
+    pic5.show()
+   }
+   function showPic6(){
+    pic6.show()
+   }
+
+
+   function hidePic1(){
+       pic1.hide();
+   }
+   function hidePic2(){
+    pic2.hide();
+}
+function hidePic3(){
+    pic3.hide();
+}
+function hidePic4(){
+    pic4.hide();
+}
+function hidePic5(){
+    pic5.hide();
+}
+function hidePic6(){
+    pic6.hide();
+}
